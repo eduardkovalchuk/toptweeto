26875f974caa7363cddcd7b1e8300b326b713cf6
Eduard Kovalchuk <edward.kovlachuk11@gmail.com>
2018-02-22 20:20:36 +0200

    rest api created

--- rest_api/rest_app.py
+++ rest_api/rest_app.py
@@ -2,38 +2,58 @@
 This is an rest API for toptweet app based on Flask microframework
 """
 
-import sys
+#--------------------------------------------------------------------------------------------------
 
+import sys
 from flask import Flask, request, jsonify
 
 sys.path.append("..")
 
 from resources import config
-from control import controllers
+from control.puppeteer import Puppeteer
+
+#--------------------------------------------------------------------------------------------------
 
 REST_API = config.REST_API
-DELETE = config.DELETE
+DELETE_HASHTAGS = config.DELETE
 ADD_HASHTAGS = config.ADD_HASHTAGS
 GET_HASHTAGS = config.GET_HASHTAGS
 GET_TOP_TWEETS = config.GET_TOP_TWEETS
 
-db_controller = controllers.DbController
+#--------------------------------------------------------------------------------------------------
 
 toptweet = Flask(__name__)
 
+puppeteer = Puppeteer()
+
+#--------------------------------------------------------------------------------------------------
 
 @toptweet.route(REST_API + ADD_HASHTAGS, methods = ['POST'])
 def add_hashtags():
-    raw_data = request.data
-    hashtags_list = raw_data.split()
-    db_controller.write_hashtags(hashtags_list)
-    return db_controller.status
+    hashtags = request.get_json()
+    status = puppeteer.write_hashtags(hashtags)
+    return jsonify(status)
+
+
+@toptweet.route(REST_API + DELETE_HASHTAGS, methods = ['DELETE'])
+def delete_hashtags():
+    hashtags = request.get_json()
+    status = puppeteer.delete_hashtags(hashtags)
+    return jsonify(status)
 
 
 @toptweet.route(REST_API + GET_HASHTAGS, methods = ['GET'])
 def get_hashtags():
-    hashtags = db_controller.get_hashtags()
+    hashtags = puppeteer.get_hashtags()
     return jsonify(hashtags)
+
+
+@toptweet.route(REST_API + GET_TOP_TWEETS, methods = ['GET'])
+def get_top_tweets():
+    top_tweets = puppeteer.get_top_tweets()
+    return jsonify(top_tweets)   
+
+#--------------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
     toptweet.run(host='0.0.0.0', port=5000, debug=True)
