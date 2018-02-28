1deb3c476a9480e0146dd89a0bf54b1b7cbf1f2d
Eduard Kovalchuk <edward.kovlachuk11@gmail.com>
2018-02-28 11:28:32 +0200

    renewing branch after error

---
+++ control/puppeteer.py
@@ -0,0 +1,2 @@
+class Puppeteer(object):
+    pass
---
+++ resources/config.py
@@ -0,0 +1,7 @@
+REST_API = '/rest/v1.0/'
+DELETE_HASHTAGS = 'delete'
+ADD_HASHTAGS = 'hashtags/add'
+GET_HASHTAGS = 'hashtags'
+GET_TOP_TWEETS = 'tweets/top'
+
+DB_IP = '192.168.128.232:27017'
